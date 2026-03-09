cdde8a9ab5d64249e792639a68094f99b27942e2
ayush00git <ayush.2007.iit@gmail.com>
2026-03-09 16:04:57 +0530

    feat(server): initialized the server and redis pub/sub

---
+++ server/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/ayush00git/lowkey/proto/v1"
+)
+
+type server struct {
+	pb.UnimplementedSignalingServer
+	router *Router
+}
+
+func newServer(redisAddr string) *server {
+	return &server{
+		router: NewRouter(redisAddr),
+	}
+}
+
+func (s *server) Connect(stream pb.Signaling_ConnectServer) error {
+	ctx := stream.Context()
+
+	// 1. Initial Identity Registration
+	req, err := stream.Recv()
+	if err != nil {
+		return err
+	}
+
+	reg := req.GetRegistration()
+	if reg == nil {
+		return fmt.Errorf("initial message must be identity registration")
+	}
+
+	clientUUID := reg.Uuid
+	log.Printf("Client connected: %s", clientUUID)
+
+	// 2. Subscribe to Redis for this client
+	pubsub := s.router.Subscribe(ctx, clientUUID)
+	defer pubsub.Close()
+
+	ch := pubsub.Channel()
+
+	// 3. Bidirectional logic
+	errCh := make(chan error, 2)
+
+	// Goroutine: Redis -> gRPC Client
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case msg, ok := <-ch:
+				if !ok {
+					return
+				}
+
+				var payload SignalPayload
+				if err := json.Unmarshal([]byte(msg.Payload), &payload); err != nil {
+					log.Printf("Error unmarshaling redis payload: %v", err)
+					continue
+				}
+
+				resp := &pb.SignalResponse{}
+				switch payload.Type {
+				case "sdp":
+					var sdp pb.SdpExchange
+					json.Unmarshal(payload.Data, &sdp)
+					resp.Payload = &pb.SignalResponse_Sdp{Sdp: &sdp}
+				case "ice":
+					var ice pb.IceCandidate
+					json.Unmarshal(payload.Data, &ice)
+					resp.Payload = &pb.SignalResponse_Ice{Ice: &ice}
+				}
+
+				if err := stream.Send(resp); err != nil {
+					errCh <- err
+					return
+				}
+			}
+		}
+	}()
+
+	// Goroutine: gRPC Client -> Redis
+	go func() {
+		for {
+			req, err := stream.Recv()
+			if err == io.EOF {
+				errCh <- nil
+				return
+			}
+			if err != nil {
+				errCh <- err
+				return
+			}
+
+			switch p := req.Payload.(type) {
+			case *pb.SignalRequest_Sdp:
+				if err := s.router.ForwardSDP(ctx, clientUUID, p.Sdp); err != nil {
+					log.Printf("Error forwarding SDP: %v", err)
+				}
+			case *pb.SignalRequest_Ice:
+				if err := s.router.ForwardICE(ctx, clientUUID, p.Ice); err != nil {
+					log.Printf("Error forwarding ICE: %v", err)
+				}
+			}
+		}
+	}()
+
+	return <-errCh
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterSignalingServer(s, newServer("localhost:6379"))
+
+	reflection.Register(s)
+
+	log.Printf("Server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
